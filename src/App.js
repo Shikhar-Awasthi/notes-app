d1120d87dac316f3bac00d066bb13aac893f4ce1
Shikhar Awasthi <shikharawasthi44@gmail.com>
2023-10-16 11:47:28 +0530

    Added the basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useId, useState } from "react";
+import "./App.css";
+import {MdDelete} from 'react-icons/md';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState("");
+  
+  const addNote = () => {
+    if(note.trim()){
+      const data = {id: crypto.randomUUID(), text: note.trim(), date: new Date().toLocaleDateString()}
+      setNotes(pre => [...pre, data]);
+      setNote("");
+    }
+  }
+  const deleteNote = (id) => {
+    setNotes(pre => pre.filter(p => p.id !== id));
+  }
+  useEffect(() => {
+    if(notes.length > 0)
+    localStorage.setItem("notes-app", JSON.stringify(notes));
+  }, [notes]);
+
+  useEffect(() => {
+    const n = localStorage.getItem("notes-app");
+    if(n) setNotes(JSON.parse(n));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="notes-list">
+      {notes.map((n) => (
+        <div key={n.id} className="note">
+          <p>{n.text}</p>
+          <p className="note-footer">{n.date} <MdDelete className="icon" onClick={() => deleteNote(n.id)} /> </p>
+        </div>
+      ))}
+      <div className="note note-add">
+        <textarea value={note} onChange={(e) => setNote(e.target.value)} placeholder="Write a note"></textarea>
+        <div className="note-footer">
+          <span>{note.trim().length}/200</span>
+          <button className="save-button" onClick={addNote}>save</button>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
