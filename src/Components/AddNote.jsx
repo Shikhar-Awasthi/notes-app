afca5c9c714edac068f404517377e6bc16f23701
Shikhar Awasthi <shikharawasthi44@gmail.com>
2023-10-26 20:03:14 +0530

    internalization

--- src/Components/AddNote.jsx
+++ src/Components/AddNote.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
 import { MdSave } from "react-icons/md";
 import NoteContext from "../NoteContext";
+import { useTranslation } from "react-i18next";
 
 function AddNote() {
   const { note, setNote, addNote } = useContext(NoteContext);
+  const { t } = useTranslation();
   return (
     <div className="note note-add">
-      <textarea value={note} onChange={(e) => setNote(e.target.value)} placeholder="Write a note"></textarea>
+      <textarea value={note} onChange={(e) => setNote(e.target.value)} placeholder={t("write a note")}></textarea>
       <div className="note-footer">
         <span>{note.trim().length}/200</span>
         <MdSave className="save-button" onClick={addNote} color="#fff" size="16px" cursor="pointer" />
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,17 +1,23 @@
 import React, { useContext } from "react";
-import { MdSearch } from "react-icons/md";
+import { MdLanguage, MdSearch } from "react-icons/md";
 import NoteContext from "../NoteContext";
+import { useTranslation } from "react-i18next";
 
 function Search() {
   const { search, setSearch } = useContext(NoteContext);
+  const { t, i18n } = useTranslation();
+  const changeLanguage = () => i18n.changeLanguage(i18n.language === "en" ? "hi" : "en");
   return (
     <div className="search-container">
       <div className="search">
         <div>
+          <MdLanguage size="1.3rem" color="#fff" onClick={changeLanguage} />
+        </div>
+        <div>
           <MdSearch size="1.3rem" color="#fff" />
         </div>
         <div>
-          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search" />
+          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder={t("search")} />
         </div>
       </div>
     </div>
